0d9123c1f1b3f2d002b2b77cd37e8e8e4ad3240d
Hussain <hussainkadiwal100@gmail.com>
2026-01-13 23:27:35 +0530

    Initial commit: Django E-Learning project

---
+++ educa/cources/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+
+        def __str__(self):
+            return self.title
+
+class Course(models.Model):
+    owner = models.ForeignKey(
+        User,
+        releted_name='courses_created',
+        on_delete=models.CASCADE,
+    )
+    subject = models.ForeignKey(
+        subject,
+        releted_name='courses',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    owerview = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        ordering = ['-created']
+
+
+    def __str__(self):
+        return self.title
+
+class Module(models.Model):
+    course = models.ForeignKey(
+        Course, releted_name='models', on_delete=models.CASCADE
+    )
+    title = models.CharFieldField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
