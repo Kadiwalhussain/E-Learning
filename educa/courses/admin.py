ffa85b9008b2c555e8baec5d571c2a8c3305b217
Hussain <hussainkadiwal100@gmail.com>
2026-01-15 12:07:24 +0530

    Add proxy models and ItemBase content types (TextItem, File, Image, Video)

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Subject, Course, Module, Text
+from .models import (
+    Subject, Course, Module, Text, ContentBase, OrderedContent,
+    TextItem, File, Image, Video
+)
 
 
 class ModuleInline(admin.StackedInline):
@@ -25,4 +28,50 @@ class CourseAdmin(admin.ModelAdmin):
 class TextAdmin(admin.ModelAdmin):
     list_display = ['title', 'created']
     search_fields = ['title', 'body']
+
+
+@admin.register(ContentBase)
+class ContentBaseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created']
+    search_fields = ['title']
+
+
+@admin.register(OrderedContent)
+class OrderedContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created', 'created_delta']
+    search_fields = ['title']
+    # Note: OrderedContent is a proxy model, so it operates on the same
+    # database table as ContentBase but with different ordering and methods
+
+
+@admin.register(TextItem)
+class TextItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    search_fields = ['title', 'content']
+    raw_id_fields = ['owner']
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    search_fields = ['title']
+    raw_id_fields = ['owner']
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    search_fields = ['title']
+    raw_id_fields = ['owner']
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'url', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    search_fields = ['title', 'url']
+    raw_id_fields = ['owner']
 
--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 
 
 class BaseContent(models.Model):
@@ -66,15 +67,84 @@ class Content(models.Model):
     module = models.ForeignKey(
         Module,
         related_name='contents',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in':('text', 'video', 'image', 'file')
+        }
     )
     content_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
-        limit_choices_to={'model__in': ('text',)}
+        limit_choices_to={'model__in': ('text', 'textitem', 'file', 'image', 'video')}
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
 
 
+# Proxy model example
+# A concrete BaseContent model for proxy inheritance demonstration
+# Note: This is separate from the abstract BaseContent above
+class ContentBase(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = 'courses_contentbase'
+
+    def __str__(self):
+        return self.title
+
+
+class OrderedContent(ContentBase):
+    class Meta:
+        proxy = True
+        ordering = ['created']
+
+    def created_delta(self):
+        return timezone.now() - self.created
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class TextItem(ItemBase):
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+    def __str__(self):
+        return self.title
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+    def __str__(self):
+        return self.title
+
+
+class Video(ItemBase):
+    url = models.URLField()
+
+    def __str__(self):
+        return self.title
